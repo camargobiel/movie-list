ff1120c58cdb6701563d45b21069811ed2775700
camargobiel <camargoobiel@gmail.com>
2022-12-04 17:05:04 -0300

    feat: added edit function

--- actions.py
+++ actions.py
@@ -20,9 +20,26 @@ def read_database():
 def remove_from_database(current_movie, filename='database.json'):
   with open(filename, 'r+') as file:
     file_data = json.load(file)
-    current_movie = current_movie.replace("\'", "\"")
+
+    if type(current_movie) is not dict:
+      current_movie = current_movie.replace("\'", "\"")
+    else:
+      current_movie = json.dumps(current_movie)
 
     file_data['movies'].remove(json.loads(current_movie))
 
   with open(filename, 'w') as file:
     json.dump(file_data, file, indent=2)
+
+def edit_database(current_movie, edited_movie, filename='database.json'):
+  with open(filename, 'r+') as file:
+    movies = json.load(file)
+    movies = movies['movies']
+
+    for movie in movies:
+      print(movie)
+      print(current_movie)
+      if str(movie) == str(current_movie):
+        remove_from_database(movie)
+        write_database(edited_movie)
+        return movie
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
-from actions import write_database, read_database, remove_from_database
+from actions import write_database, read_database, remove_from_database, edit_database
+import json
 
-movies = []
 movies_types = ['Terror', 'Ação', 'Animação', 'Comédia', 'Romance', 'Drama']
 movies_classification = ['L', '10', '12', '14', '16', '18']
 
@@ -25,12 +25,32 @@ def create():
 
 @app.route('/create_page')
 def create_page():
-  return render_template('create_movie.html', movies_types=movies_types, movies_classification=movies_classification)
+  return render_template('movie.html', movies_types=movies_types, movies_classification=movies_classification)
+
+@app.route('/edit_page', methods=['POST'])
+def edit_page():
+  current_movie = request.form['movie']
+  movie = json.loads(current_movie.replace("\'", "\""))
+
+  return render_template('edit_movie.html', movies_types=movies_types, movies_classification=movies_classification, movie=movie)
+
+@app.route('/edit', methods=['POST'])
+def edit():
+  current_movie = request.form['current_movie']
+  movie_name = request.form['movie_name']
+  movie_type = request.form['movie_type']
+  movie_classification = request.form['movies_classification']
+
+  edited_movie = {'name': movie_name, 'movie_type': movie_type, 'movie_classification': movie_classification}
+
+  edit_database(current_movie, edited_movie)
+
+  return redirect_to_home()
 
 @app.route('/remove', methods=['POST'])
 def remove():
   current_movie = request.form['movie']
-  movies = remove_from_database(current_movie)
+  remove_from_database(current_movie)
 
   return redirect_to_home()
 
